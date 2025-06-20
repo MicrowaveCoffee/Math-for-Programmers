d2b88bf45b8edf9c4688e7fdb1974f425c58e0b5
MicrowaveCoffee <32424042+MicrowaveCoffee@users.noreply.github.com>
2025-06-19 21:15:36 -0500

    vector transformation

--- Chapter 04/draw.py
+++ Chapter 04/draw.py
@@ -1,4 +1,24 @@
 from teapot import load_triangles
 from draw_model import draw_model
 
-draw_model(load_triangles())
+from vectors import *
+
+def scale2(v):
+    return scale(2.0,v)
+
+def tranlate_left(v):
+    return add((-1,0,0),v)
+
+#Composite function
+def scale_to_translate(v):
+    return tranlate_left(scale2(v))
+
+regular_triangles = load_triangles()
+scaled_triangles = [
+    [scale_to_translate(vertex) for vertex in triangle]
+    for triangle in regular_triangles
+]
+
+
+
+draw_model(scaled_triangles)
