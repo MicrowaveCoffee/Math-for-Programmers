e6b708b5c5005572c20e968343d8742a4b4a1042
MicrowaveCoffee <32424042+MicrowaveCoffee@users.noreply.github.com>
2025-06-19 21:35:29 -0500

    cleaner composite function

--- Chapter 04/draw.py
+++ Chapter 04/draw.py
@@ -10,12 +10,19 @@ def tranlate_left(v):
     return add((-1,0,0),v)
 
 #Composite function
-def scale_to_translate(v):
-    return tranlate_left(scale2(v))
+# def scale_to_translate(v):
+#     return tranlate_left(scale2(v))
+
+def compose(f1,f2):
+    def new_function(input):
+        return f1(f2(input))
+    return new_function
+
+scale2_then_translate_left = compose(tranlate_left,scale2)
 
 regular_triangles = load_triangles()
 scaled_triangles = [
-    [scale_to_translate(vertex) for vertex in triangle]
+    [scale2_then_translate_left(vertex) for vertex in triangle]
     for triangle in regular_triangles
 ]
 
